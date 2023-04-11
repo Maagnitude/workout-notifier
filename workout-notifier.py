e98fa9a90b08277d7c6e1cddc34838f9c1d7a447
Maagnitude <yiwrgos_v.i.p@hotmail.com>
2023-04-11 17:28:09 +0300

    fix: bug in hour/minute. Now works perfectly

--- workout-notifier.py
+++ workout-notifier.py
@@ -25,19 +25,26 @@ video = YouTube(url)                # We make a YouTube video object, to use its
 
 video_title = video.title           # YouTube video's title
 video_author = video.author         # YouTube video's creator
+video_length = video.length         # YouTube video's length-duration
+
+video_minutes = video_length // 60
+video_seconds = video_length % 60
+
+# We get the current time
+current_time = time.localtime()
 
 # In this section we set the time we want the script to open the video (24-hour format)
-print("\nDefault hour/minute: 14:45\n")
+print("\nDefault hour/minute: 15:50\n")
 hour = input("Enter the hour of the day for the reminder (or press Enter): ")
 minute = input("\nEnter the minute of the day for the reminder (or press Enter): ")
 
-if hour == '':              # If no hour given, default is 14
-    hour = 14
+if hour == '':              # If no hour given, default is 15
+    hour = 15
 else:
     hour = int(hour)        # otherwise we typecast it to int
 
-if minute == '':            # If no minute given, default is 45
-    minute = 45
+if minute == '':            # If no minute given, default is 50
+    minute = 50
 else:
     minute = int(minute)    # otherwise we typecast it to int
     
@@ -45,11 +52,11 @@ minute_vis = '0' + str(minute) if minute < 10 else minute   # If the minute is b
 
 print(f"\nWorkout reminder has been set!\nYou'll watch '{Fore.GREEN}{video_title}{Fore.RESET}' \n  \
         by {Fore.RED}'{video_author}'{Fore.RESET} \n  \
-        at {Fore.BLUE}{hour}:{minute_vis}{Fore.RESET}!\n  \
-        Be prepared!")
+        at {Fore.BLUE}{hour}:{minute_vis}:{time.localtime().tm_sec if time.localtime().tm_sec>9 else '0'+str(time.localtime().tm_sec)}{Fore.RESET}!\n  \
+        Be prepared to dedicate {Fore.GREEN}{video_minutes} minutes and {video_seconds} seconds{Fore.RESET} of your time!")
 
 while True:
-    # We get the current time
+    # We get the current time again to work inside the loop
     current_time = time.localtime()
 
     # If the current time is equal to the desired time, we open the YouTube video
@@ -57,12 +64,18 @@ while True:
         
         webbrowser.open(url)        # Opening the URL in the default web browser
         
-        time.sleep(2)               # Waiting 2 seconds to load the video
+        time.sleep(4)               # Waiting 4 seconds to load the video
         
         pyautogui.press('f')        # Pressing 'f' key, to get into fullscreen
         
         print(f"\nYour video: '{Fore.GREEN}{video_title}{Fore.RESET}' just started!\nHave fun!")
+        
+        time.sleep(video_length)
+        
+        pyautogui.hotkey('ctrl', 'w')   # Closing the tab
+        
+        print(f"\nVideo-Workout ended! Tab closed!\nNext workout same time tomorrow! Have a good day!\n")
 
-        time.sleep(24 * 60 * 60)    # Waiting exactly 24 hours, to check the time again
+        time.sleep(24 * 60 * 60 - video_length)    # Waiting exactly 24 hours minus the length of the video, to check the time again
     else:
         time.sleep(60)              # Checking the time every 60 seconds
